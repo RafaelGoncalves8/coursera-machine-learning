de5f7239db0bba9e22d57a081ac507ba992fe84f
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-12-11 11:27:06 -0200

    univariate implementations of compute cost and gradient descent apply for multivariate

--- 01-linear_regression/ex1/gradientDescentMulti.m
+++ 01-linear_regression/ex1/gradientDescentMulti.m
@@ -17,15 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    h = X*theta;
 
-
-
-
-
-
-
-
-
+    % theta_j := theta_j - alpha/m*[sum((h(xi)-yi)*xi_j)]
+    theta = theta - alpha*(1/m)*(X'*(h - y));
 
     % ============================================================
 
