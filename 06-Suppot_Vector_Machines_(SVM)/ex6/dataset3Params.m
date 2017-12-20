ec0523300b55bb4d026c865f93455e863a0adaec
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-12-20 16:59:58 -0200

    done finding C and sigma

--- 06-Suppot_Vector_Machines_(SVM)/ex6/dataset3Params.m
+++ 06-Suppot_Vector_Machines_(SVM)/ex6/dataset3Params.m
@@ -7,7 +7,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
+% You need to return the following variables correctly.  
 C = 1;
 sigma = 0.3;
 
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Best parameters found by this algorithm (algorithm commented 
+% because each time I run submit this code would run also and 
+% this code is very time consuming). 
+C = 1;
+sigma = 0.1;
 
-
-
-
-
+% Uncomment for the algorithm I used to find best C and sigma
+% options = [0.01 0.03 0.1 0.3 1 3 10 30];
+% model = @(C, sigma) svmTrain(X ,y ,C , ...
+%                     @(x1,x2) gaussianKernel(x1, x2, sigma));
+% err = @(x) mean(double(x ~= yval));
+% predictions = [];
+%
+% for i = 1:8,
+%     for j = 1:8,
+%         C = options(i); sigma = options(j);
+%         pred = svmPredict(model(C, sigma), Xval);
+%         predictions = [predictions; C sigma err(pred)];
+%     end;
+% end
+%
+% predictions
+% [v, index] = min(predictions(:,3)) 
+%
+% C = predictions(index, 1)
+% sigma = predictions(index, 2)
 
 % =========================================================================
 
